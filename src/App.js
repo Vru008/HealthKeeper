7ecabb46587f7be2365cf3e51497a382e85cf94e
khushi <patelk148pk@gmail.com>
2023-03-29 12:09:47 +0530

    commit1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hosp from './Pages/Hospital/Hosp'
+// import './Pages/Hospital/hosp.css'
+
+function App() {
+  return (
+    <Hosp/>
+  );
+}
+
+export default App;
