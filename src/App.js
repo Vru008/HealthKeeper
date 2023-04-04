7a6a084eaf366a47fd5b77e01e5aa4ab8b936958
Vru008 <vruttantpatel00@gmail.com>
2023-04-04 11:43:13 +0530

    add db

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import AboutUs from "./Pages/AboutUs/AboutUs";
 import "./Pages/AboutUs/aboutUs.css";
 import Department from "./Pages/Department/Department";
 import "./Pages/Department/department.css";
-import Home from "./Pages/home/Home";
+import Home from "./Pages/Home/Home";
 import Doctor from "./Pages/Doctor/Doctor";
 import "./Pages/Doctor/doctor.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+      {/* <Routes>
+          <Route index element={<Home />} />
+          <Route path="AboutUs" element={<AboutUs />} />
+          <Route path="contact" element={<contact />} />
+          <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter> */}
+      {/* <Home/>
       <AboutUs />
       <Department />
-      <Doctor />
+      <Doctor /> */}
     </>
   );
 }
--- src/Pages/AboutUs/aboutUs.js
+++ src/Pages/AboutUs/aboutUs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../AboutUs/aboutUs.css'
+
+
 const AboutUs = () => {
   return (
 
